27c452cedf5c7a6900ab2bce260d20beccfbcbaa
drony <drony2@mail.ru>
2023-11-28 03:40:30 +0300

    Добавьте файлы проекта.

---
+++ 16/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _16
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    ///  Разработайте страницу добавления товара для информационной системы «Мебельный салон. Реализуйте проверку на отсутствие пустых полей при добавлении нового товара.
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ClassHelper.navFrame = contextFrame;
+            ClassHelper.navFrame.Navigate(new Page1());
+        }
+    }
+}
---
+++ 16/Page1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _16
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (txbAppointment.Text !="" &&  txbColor.Text!= "" && txbheight.Text != "" && txblength.Text != "" && txbManufacturer.Text != "" && txbNameProduct.Text != "" && txbwidth.Text != "" &&)
+            {
+                MessageBox.Show("Товар добавлен!!!");
+            }
+            else
+            {
+                MessageBox.Show("Не все поля были заполнены");
+
+            }
+        }
+    }
+}
